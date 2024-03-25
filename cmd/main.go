1056de14f3ec9f1fa5be7133c57bbae3c70b8297
Rafał Schmidt <rafalschmidt@Rafas-MacBook-Pro.local>
2024-03-25 22:15:33 +0100

    feat: contact list

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io"
 
@@ -23,7 +24,35 @@ func newTemplates() *templates {
 }
 
 type IndexModel struct {
-	Count int
+	FormErrorMessage string
+	Contacts         []Contact
+}
+
+func (indexModel *IndexModel) addContact(name, address string) (contact Contact, error error) {
+	if indexModel.contactExists(address) {
+		indexModel.FormErrorMessage = "Contact with this address already exists"
+		return Contact{}, errors.New("exists")
+	}
+
+	newContact := Contact{Name: name, Address: address}
+	indexModel.Contacts = append(indexModel.Contacts, newContact)
+	indexModel.FormErrorMessage = ""
+	return newContact, nil
+}
+
+func (indexModel *IndexModel) contactExists(address string) bool {
+	for _, v := range indexModel.Contacts {
+		if v.Address == address {
+			return true
+		}
+	}
+
+	return false
+}
+
+type Contact struct {
+	Name    string
+	Address string
 }
 
 func main() {
@@ -32,13 +61,29 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Renderer = newTemplates()
 
-	indexModel := IndexModel{Count: 0}
+	indexModel := IndexModel{
+		Contacts: []Contact{
+			{Name: "Hello", Address: "h@w.pl"},
+			{Name: "World", Address: "w@w.pl"},
+		},
+	}
+
 	e.GET("/", func(c echo.Context) error {
+		indexModel.FormErrorMessage = ""
 		return c.Render(200, "index", indexModel)
 	})
-	e.POST("/count", func(c echo.Context) error {
-		indexModel.Count++
-		return c.Render(200, "counter", indexModel)
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		address := c.FormValue("address")
+		_, ce := indexModel.addContact(name, address)
+
+		if ce != nil {
+			return c.Render(409, "form-error", indexModel)
+		}
+
+		return c.Render(200, "contacts", indexModel)
+		// return c.Render(200, "oob-contact", co)
 	})
 
 	e.Logger.Fatal(e.Start(":8081"))
